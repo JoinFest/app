4f193c47815c56e1e5e8ce6c42212dbec30f3450
violetow9 <violetow9@gmail.com>
2024-06-12 10:34:17 +0200

    feat: added database, config, passport, jwt modules & all service domains

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,16 +1,45 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
+import {Module} from '@nestjs/common';
+import {JwtModule} from '@nestjs/jwt';
+import {PassportModule} from '@nestjs/passport';
+import {ConfigModule, ConfigService} from '@nestjs/config';
+import {UserController} from './interfaces/controllers/user.controller';
+import {EventController} from './interfaces/controllers/event.controller';
+import {AuthController} from './interfaces/controllers/auth.controller';
+import {UserService} from './application/services/user.service';
+import {EventService} from './application/services/event.service';
+import {AuthService} from './application/services/auth.service';
+import {UserRepository} from './domain/repositories/user.repository';
+import {EventRepository} from './domain/repositories/event.repository';
+import {DatabaseModule} from './infrastructure/database/mongoose.module';
+import {LocalStrategy} from './infrastructure/auth/local.strategy';
+import {JwtStrategy} from './infrastructure/auth/jwt.strategy';
+import {JwtAuthGuard} from "./interfaces/guards/jwt-auth.guard";
 
 @Module({
-  imports: [
-    MongooseModule.forRoot('mongodb://localhost/nestjs-app'),
-    UserModule,
-    AuthModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        DatabaseModule,
+        ConfigModule.forRoot(),
+        PassportModule,
+        JwtModule.registerAsync({
+            imports: [ConfigModule],
+            useFactory: async (configService: ConfigService) => ({
+                secret: configService.get<string>('JWT_SECRET'),
+                signOptions: {expiresIn: '60m'},
+            }),
+            inject: [ConfigService],
+        }),
+    ],
+    controllers: [UserController, EventController, AuthController],
+    providers: [
+        UserService,
+        EventService,
+        AuthService,
+        UserRepository,
+        EventRepository,
+        LocalStrategy,
+        JwtStrategy,
+        JwtAuthGuard,
+    ],
 })
-export class AppModule {}
+export class AppModule {
+}
