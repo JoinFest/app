bd1588c6a5fe02949ceb29c0d08c29e0526e3ccc
violetow9 <violetow9@gmail.com>
2024-06-12 10:40:19 +0200

    chore: added validation pipe & swagger module builder

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,29 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {ValidationPipe} from "@nestjs/common";
+import {AllExceptionsFilter} from "./common/filters/all-exceptions.filter";
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+
+    app.useGlobalPipes(new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+    }));
+    app.useGlobalFilters(new AllExceptionsFilter());
+
+    const config = new DocumentBuilder()
+        .setTitle('API Documentation')
+        .setDescription('The API description')
+        .setVersion('1.0')
+        .addBearerAuth()
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
+    await app.listen(3000);
 }
+
 bootstrap();
