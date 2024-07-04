77d6368665c524e1456396f98d59aa92ba9d9504
Moustapha yaya sow <Sowmoustaphayaya@gmail.com>
2024-07-04 06:08:27 +0200

    v1 front include the basement of the project

---
+++ frontend/Front_b/script.js
@@ -0,0 +1,25 @@
+// script.js
+
+function buyTicket() {
+    alert("Fonction d'achat de ticket à implémenter.");
+}
+
+function inviteFriends() {
+    alert("Fonction d'invitation d'amis à implémenter.");
+}
+
+function showEventDetails(eventId) {
+    alert(`Afficher les détails pour l'événement: ${eventId}`);
+}
+
+function viewProfile(friendId) {
+    alert(`Afficher le profil pour: ${friendId}`);
+}
+
+function inviteToEvent(friendId) {
+    alert(`Inviter ${friendId} à un événement`);
+}
+
+function showQrCode(eventId) {
+    alert(`Afficher le QR Code pour l'événement: ${eventId}`);
+}
