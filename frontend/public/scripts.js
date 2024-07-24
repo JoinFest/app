99dcd60b572a767533d37ee9d10a3c0794b605f6
Moustapha yaya sow <Sowmoustaphayaya@gmail.com>
2024-07-24 15:54:23 +0200

    Updtae login & Register page & arch organisation

--- frontend/public/scripts.js
+++ frontend/public/scripts.js
@@ -31,4 +31,25 @@ document.addEventListener('DOMContentLoaded', () => {
         event.preventDefault();
         alert("Informations du profil mises à jour !");
     });
+
+     const loginForm = document.getElementById("login-form");
+
+    loginForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        const username = document.getElementById("username").value.trim();
+        const password = document.getElementById("password").value.trim();
+
+        if (username === "" || password === "") {
+            alert("Tous les champs sont requis.");
+            return;
+        }
+
+        // Validation simulée des informations de connexion
+        // Ici vous pouvez ajouter des appels API pour vérifier les informations de connexion
+        
+        // Si la validation est réussie
+        alert("Connexion réussie !");
+        window.location.href = "home.html";
+    });
 });
